5f86a718418e0f84d2038b0f6672ae0065a3f87a
Ryan McNally <therealryan@gmail.com>
2015-01-29 21:14:50 +0000

    tests dir added

---
+++ src/main/java/com/rmn/gdxtend/Foo.java
@@ -0,0 +1,5 @@
+package com.rmn.gdxtend;
+
+public class Foo {
+
+}
---
+++ src/test/java/com/rmn/gdxtend/FooTest.java
@@ -0,0 +1,12 @@
+package com.rmn.gdxtend;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FooTest {
+
+	@Test
+	public void test() {
+		Assert.fail( "ruhroh!" );
+	}
+}
